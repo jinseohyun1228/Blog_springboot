a911bc92f80c6d872631907b84cfe6082732d665
JinSeoHyun <15roa1121@gmail.com>
2024-02-19 01:07:15 +0900

    환경 설정

---
+++ demo/src/test/java/com/jshblog/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshblog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
